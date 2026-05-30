ec2084f6ecc4083ed3da75b8747c361b6b69eaeb
sajjad006 <mailsajjad006@gmail.com>
2026-05-30 22:24:24 +0530

    added fantastic new feature

--- demo.py
+++ demo.py
@@ -3,6 +3,8 @@ print ("hello git!!!!!")
 
 print ("i have changed this file now!!!!")
 
+print("added some new features... this is fantastic...!")
+
 # working directory  -> git add .
 
 # staging area -> git commit -m "added demo.py" 
