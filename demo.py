8d984a32b64a71d1401452b03f00a1d87697d1ad
sajjad006 <mailsajjad006@gmail.com>
2026-05-30 22:26:51 +0530

    Revert "added fantastic new feature"
    This reverts commit ec2084f6ecc4083ed3da75b8747c361b6b69eaeb.

--- demo.py
+++ demo.py
@@ -3,8 +3,6 @@ print ("hello git!!!!!")
 
 print ("i have changed this file now!!!!")
 
-print("added some new features... this is fantastic...!")
-
 # working directory  -> git add .
 
 # staging area -> git commit -m "added demo.py" 
