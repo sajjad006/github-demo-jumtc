9fac31c8e5e90bac4a4790af4d3e31f38348fcfa
sajjad006 <mailsajjad006@gmail.com>
2026-05-30 22:23:38 +0530

    made some changes...

--- demo.py
+++ demo.py
@@ -1,9 +1,10 @@
 
 print ("hello git!!!!!")
 
+print ("i have changed this file now!!!!")
 
 # working directory  -> git add .
 
-# staging area 
-
+# staging area -> git commit -m "added demo.py" 
+      
 # commited changes / local repository
